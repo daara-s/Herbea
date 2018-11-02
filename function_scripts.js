dce39c9a348f7e766ad37ddc8a01e61974275787
Daara Shaw <daarashaw@users.noreply.github.com>
2018-11-02 11:53:40 +0000

    Add form functionality

--- function_scripts.js
+++ function_scripts.js
@@ -1,3 +1,15 @@
 function multiForm() {
-document.getElementById('form').innerHTML = "Button pressed";
+//document.getElementById('form').innerHTML = "Button pressed";
+var form = document.createElement("form");
+var node = document.createElement("input");
+node.setAttribute("type", "text");
+
+form.appendChild(node);
+document.getElementById("form-container").appendChild(form);
 }
+
+var form = document.createElement("form");
+var node = document.createElement("input");
+node.setAttribute("type", "text");
+
+form.appendChild(node);
